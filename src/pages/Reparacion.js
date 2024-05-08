562385f30fefff6e15834fa2c09c5030fc046b0b
Ricardo Segura <seguraricardo380@gmail.com>
2024-05-08 14:25:11 -0500

    feat: Servicio tecnico seccion Informacion

--- src/pages/Reparacion.js
+++ src/pages/Reparacion.js
@@ -1,7 +1,45 @@
 const Reparacion = () => {
-	return <>
+	return <div>
+		<section className="reparacionrs">
+			<div className="informacionrs">
+				<div className="titulors">
+					<h1>SERVICIO TÉCNICO, REPARACIÓN, ARREGLO Y MANTENIMIENTO DE LAVADORAS A
+						DOMICILIO</h1>
+				</div>
+				<div className="contenedorrs">
+					<div className="contenedor2rs">
+						<div>
+							<h2>Reparación y Servicio Técnico de Lavadoras Rapido, Efectivo y
+								Economico</h2>
+							<p>Lavaexpress es una empresa multimarca independiente, experta en
+								reparación, soporte técnico y mantenimiento de lavadoras y
+								secadoras
+								del hogar. <br />
+								El servicio que ofrecemos es con técnicos profesionales y
+								certificados para
+								brindar soluciones a cualquier reparación o servicio que requieras
+								con
+								resultados garantizados y de alta calidad.
+								<br />
+								Nuestros técnicos están calificados para realizar reparaciones y
+								dar
+								servicio de
+								mantenimiento a diversas marcas comerciales, somos una empresa
+								multimarca.
+								Llámanos y realizamos una visita a tu domicilio para un
+								diagnóstico.
+							</p>
+						</div>
+						<div>
+							<h2>Área de servicio y reparación premium.</h2>
+							<img src="reparacion.webp" alt="imagene de una reparacion" />
+						</div>
 
-	</>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 }
 
 export default Reparacion;
