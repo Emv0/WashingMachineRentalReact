6ef3dbc46c6b686b231a3d9e6a63adeb4979dd62
Esteban <emv31028@gmail.com>
2024-06-12 22:07:07 -0500

    Correción del proyecto

--- react-front/src/pages/Inicio.js
+++ react-front/src/pages/Inicio.js
@@ -1,74 +1,104 @@
-const Inicio = ()=>{
-    return<main>
-    <div className="container mt-5  ">
-        <div id="carouselExampleIndicators" className="carousel slide">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="https://w.forfun.com/fetch/1e/1ec4d0939be1c224fd8ff8c7731fc484.jpeg" className="d-block w-100 carr-img" alt="..."height="500px"/>
+const Inicio = () => {
+    return <main>
+        <div className="container mt-5  ">
+            <div id="carouselExampleIndicators" className="carousel slide">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"
+                        aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
                 </div>
-                <div className="carousel-item">
-                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" className="d-block w-100" alt="..."height="500px"/>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://exitocol.vtexassets.com/arquivos/ids/23151195/Lavadora-SAMSUNG-Carga-Superior-19-kg-42-lb-WA19CG6745BVCO-3499368_d.jpg?v=638538072191330000" className="d-block w-100 carr-img" alt="..." height="685px" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://exitocol.vtexassets.com/arquivos/ids/23151186/Lavadora-SAMSUNG-Carga-Superior-18-kg-40-lb-WA18CG6745BVCO-3499367_c.jpg?v=638538072095430000" className="d-block w-100" alt="..." height="685px" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://exitocol.vtexassets.com/arquivos/ids/23150929/Lavaseca-SAMSUNG-Carga-Frontal-20-kg-44lb-WD20T6300GPCO-3271087_b.jpg?v=638538071454330000" className="d-block w-100" alt="..." height="685px" />
+                    </div>
                 </div>
-                <div className="carousel-item">
-                    <img src="https://cdn.wallpapersafari.com/13/84/wlZ31L.jpg" className="d-block w-100" alt="..."height="500px"/>
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
-    </div>
-    <section className="products-section">
-        <h2 className="products-title">Productos</h2>
-        <div className="items-container">
-            <div className="item">
-                <img className="item-image" src="https://cdn1.totalcommerce.cloud/casalimpia/product-zoom/es/jabon-liquido-multiusos-casalimpia-3785ml-1.webp" alt="" height="200px"/>
-                <h5 className="product-name">Nombre producto</h5>
-                <p className="product-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis veniam tempore a inventore laboriosam mollitia eius alias?</p>
-            </div>
-            <div className="item">
-                <img className="item-image" src="https://cdn1.totalcommerce.cloud/casalimpia/product-zoom/es/jabon-liquido-multiusos-casalimpia-3785ml-1.webp" alt="" height="200px"/>
-                <h5 className="product-name">Nombre producto</h5>
-                <p className="product-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis veniam tempore a inventore laboriosam mollitia eius alias?</p>
-            </div>                
-            <div className="item">
-                <img className="item-image" src="https://cdn1.totalcommerce.cloud/casalimpia/product-zoom/es/jabon-liquido-multiusos-casalimpia-3785ml-1.webp" alt="" height="200px"/>
-                <h5 className="product-name">Nombre producto</h5>
-                <p className="product-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis veniam tempore a inventore laboriosam mollitia eius alias?</p>
-            </div>
-            <div className="item">
-                <img className="item-image" src="https://cdn1.totalcommerce.cloud/casalimpia/product-zoom/es/jabon-liquido-multiusos-casalimpia-3785ml-1.webp" alt="" height="200px"/>
-                <h5 className="product-name">Nombre producto</h5>
-                <p className="product-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis veniam tempore a inventore laboriosam mollitia eius alias?</p>
-            </div>
-            <div className="item">
-                <img className="item-image" src="https://cdn1.totalcommerce.cloud/casalimpia/product-zoom/es/jabon-liquido-multiusos-casalimpia-3785ml-1.webp" alt="" height="200px"/>
-                <h5 className="product-name">Nombre producto</h5>
-                <p className="product-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis veniam tempore a inventore laboriosam mollitia eius alias?</p>
-            </div>                
-            <div className="item">
-                <img className="item-image" src="https://cdn1.totalcommerce.cloud/casalimpia/product-zoom/es/jabon-liquido-multiusos-casalimpia-3785ml-1.webp" alt="" height="200px"/>
-                <h5 className="product-name">Nombre producto</h5>
-                <p className="product-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis veniam tempore a inventore laboriosam mollitia eius alias?</p>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
-    </section>
-</main>
+        <section className="products-section">
+            <h2 className="products-title">Otros Productos</h2>
+            <div className="items-container">
+                <div className="item">
+                    <img className="item-image" src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/547756/w=1036,h=832,f=webp,fit=contain,q=85" alt="" height="200px" />
+                    <h5 className="product-name">Detergente Liquido Ropa Ariel</h5>
+                    <ul className="product-description">
+                        <li>Garantía : 12 meses </li>
+                        <li>Formato : Líquido</li>
+                        <li>Origen Producto : Producto Nacional</li>
+                        <li>Tipo : Jabon líquido para ropa</li>
+                    </ul>
+                </div>
+                <div className="item">
+                    <img className="item-image" src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/608567/w=1036,h=832,f=webp,fit=contain,q=85" alt="" height="200px" />
+                    <h5 className="product-name">Detergente Líquido Ropa Tide </h5>
+                    <ul className="product-description">
+                        <li>Alto : 28 cm</li>
+                        <li>Ancho : 15 cm</li>
+                        <li>Largo : 23 cm</li>
+                        <li>Tipo : Jabón líquido para ropa</li>
+                    </ul>
+                </div>
+                <div className="item">
+                    <img className="item-image" src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/606401/w=1036,h=832,f=webp,fit=contain,q=85" alt="" height="200px" />
+                    <h5 className="product-name">Desmanchador Vanish Liquido Rosa</h5>
+                    <ul className="product-description">
+                        <li>Alto : 29 cm</li>
+                        <li>Ancho : 9 cm</li>
+                        <li>Largo : 29.5 cm</li>
+                        <li>Tipo : Quitamanchas</li>
+                    </ul>
+                </div>
+                <div className="item">
+                    <img className="item-image" src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/351386/w=1036,h=832,f=webp,fit=contain,q=85" alt="" height="200px" />
+                    <h5 className="product-name">Detergente Polvo PQP Profesional Floral</h5>
+                    <ul className="product-description">
+                        <li>Garantía : 1 año</li>
+                        <li>Contenido : 10 kilogramos</li>
+                        <li>Uso : Hogar e institucional</li>
+                        <li>Aroma: Floral</li>
+                    </ul>
+                </div>
+                <div className="item">
+                    <img className="item-image" src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/490924/w=1036,h=832,f=webp,fit=contain,q=85" alt="" height="200px" />
+                    <h5 className="product-name">Detergente Liquido Ropa Tide Simply89</h5>
+                    <ul className="product-description">
+                        <li>Tipo : Detergentes líquidos</li>
+                        <li>Aroma : Original</li>
+                        <li>Presentación : Tarro</li>
+                        <li>Contenido : 4080ml</li>
+                    </ul>
+                </div>
+                <div className="item">
+                    <img className="item-image" src="https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/sodimacCO/454744/w=1036,h=832,f=webp,fit=contain,q=85" alt="" height="200px" />
+                    <h5 className="product-name">Detergente Liquido Ropa Oscura Woolite</h5>
+                    <ul className="product-description">
+                        <li>Garantía : 12 meses</li>
+                        <li>Rendimiento : 50 Lavadas</li>
+                        <li>Contenido : Galon</li>
+                        <li>Tipo : Jabones para Lavadoras</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
 }
 
 export default Inicio;
